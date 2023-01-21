2c082a55c951486660628e1523d5f6f2752ac672
Alexey Copylov <ElleryAree@gmail.com>
2023-01-21 16:29:03 +0200

    some better test checks

--- Util.py
+++ Util.py
@@ -8,6 +8,36 @@ def test(expected, actual, name=None, comparator=lambda a, e: a == e):
     else:
         print("[*] %sPassed" % name)
 
+def test_is_not_incorrect(expected, actual, name=None):
+    if name is None:
+        name = ""
+    else:
+        name = "%s: " % name
+    if expected == actual:
+        print("[x] %sFailed: %d is not the right value" % (name, expected))
+    else:
+        print("[*] %sPassed" % name)
+
+def test_is_not_too_big(expected, actual, name=None):
+    if name is None:
+        name = ""
+    else:
+        name = "%s: " % name
+    if expected <= actual:
+        print("[x] %sFailed: %d was too big. Actual was %d" % (name, expected, actual))
+    else:
+        print("[*] %sPassed" % name)
+
+def test_is_not_too_low(expected, actual, name=None):
+    if name is None:
+        name = ""
+    else:
+        name = "%s: " % name
+    if expected >= actual:
+        print("[x] %sFailed: %d was too low. Actual was %d" % (name, expected, actual))
+    else:
+        print("[*] %sPassed" % name)
+
 
 def from_file(filename):
     with open(filename) as f:
