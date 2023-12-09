a811f32ed812b0d4c8dc2feada755dc6e54b04a3
alexei-copylov <alexei.copylov@rovio.com>
2023-12-09 16:54:12 +0200

    colored test output

--- Util.py
+++ Util.py
@@ -4,9 +4,9 @@ def test(expected, actual, name=None, comparator=lambda a, e: a == e):
     else:
         name = "%s: " % name
     if not comparator(actual, expected):
-        print("[x] %sFailed: Expected %s, but was: %s" % (name, expected, actual))
+        print_bad("[x] %sFailed: Expected %s, but was: %s" % (name, expected, actual))
     else:
-        print("[*] %sPassed" % name)
+        print_good("[*] %sPassed: %s" % (name, actual))
 
 def test_is_not_incorrect(expected, actual, name=None):
     if name is None:
@@ -14,9 +14,9 @@ def test_is_not_incorrect(expected, actual, name=None):
     else:
         name = "%s: " % name
     if expected == actual:
-        print("[x] %sFailed: %d is not the right value" % (name, expected))
+        print_bad("[x] %sFailed: %d is not the right value" % (name, expected))
     else:
-        print("[*] %sPassed" % name)
+        print_good("[*] %sPassed" % name)
 
 def test_is_not_too_big(expected, actual, name=None):
     if name is None:
@@ -24,9 +24,9 @@ def test_is_not_too_big(expected, actual, name=None):
     else:
         name = "%s: " % name
     if expected <= actual:
-        print("[x] %sFailed: %d was too big. Actual was %d" % (name, expected, actual))
+        print_bad("[x] %sFailed: %d was too big. Actual was %d" % (name, expected, actual))
     else:
-        print("[*] %sPassed" % name)
+        print_good("[*] %sPassed" % name)
 
 def test_is_not_too_low(expected, actual, name=None):
     if name is None:
@@ -34,9 +34,9 @@ def test_is_not_too_low(expected, actual, name=None):
     else:
         name = "%s: " % name
     if expected >= actual:
-        print("[x] %sFailed: %d was too low. Actual was %d" % (name, expected, actual))
+        print_bad("[x] %sFailed: %d was too low. Actual was %d" % (name, expected, actual))
     else:
-        print("[*] %sPassed" % name)
+        print_good("[*] %sPassed" % name)
 
 
 def from_file(filename):
@@ -50,3 +50,9 @@ def major_adjacent():
 
 def diagonally_adjacent():
     return (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
+
+def print_good(message):
+    print("\033[92m[*] %s\033[0m" % message)
+
+def print_bad(message):
+    print("\033[91m[x] %s\033[0m" % message)
